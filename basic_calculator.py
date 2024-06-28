ec35c6d0eb77845ba7bd42256e49164aee68f9a8
Ruan Sei <luckydashy0@gmail.com>
2024-06-28 11:45:16 +0700

    Under Construction

---
+++ basic_calculator.py
@@ -0,0 +1,15 @@
+# Kalkulator sedeharna buatan Sei
+
+# Function buat penambahan, pengurangan, perkalian, pembagian, dll
+def tambah(x,y):
+  return x + y
+
+def kurang(x,y):
+  return x - y
+
+def kali(x,y):
+  return x * y 
+
+def bagi(x,y):
+  return x / y
+
