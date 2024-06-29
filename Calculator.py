7674390fc411061f82c72677976e8d5948a4ab23
ExZhonya <exzhonya@gmail.com>
2024-06-29 17:29:47 +0700

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,22 +1,20 @@
-#simple calculator made by ExZhonya(Lua)
-#Will update later for better performance
-
+#Update to make x, × and ÷ available to use, as well 'plus', 'minus'
 number1 = input("Enter first number: ")
 operation = input("Enter operation : ")
 number2 = input("Enter second number: ")
 
-if operation == '+':
+if operation in ('+', 'plus'):
     result_plus = float(number1) + float(number2)
-    print(f"Your result is : {result_plus}")
-elif operation == '-':
+    print(f"{number2} + {number2} =  {result_plus}")
+elif operation in ('-', 'minus'):
     result_minus = float(number1) - float(number2)
-    print(f"Your result is : {result_minus}")
-elif operation == '*':
+    print(f"{number1} - {number2} = {result_minus}")
+elif operation in ('*', 'x', '×'):
     result_multi = float(number1) * float(number2)
-    print(f"Your result is : {result_multi}")
-elif operation == '/':
+    print(f"{number1} × {number2} = {result_multi}")
+elif operation in ('/', '÷', ':'):
     if number2 == '0':
         print("Cannot divide by zero")
     else:
         result_division = float(number1) / float(number2)
-        print(f"Your result is : {result_division}")
+        print(f"{number1} : {number2} = {result_division}")
