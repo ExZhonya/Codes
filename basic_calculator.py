000d47ff1e2615b7f2078be44fd5039f05df1656
Ruan Sei <luckydashy0@gmail.com>
2024-06-29 16:05:50 +0700

    Update basic_calculator.py

--- basic_calculator.py
+++ basic_calculator.py
@@ -13,3 +13,19 @@ def kali(x,y):
 def bagi(x,y):
   return x / y
 
+# Ambil Input Dari User
+ambilInputX = input("Masukan Nomor ke-1: ")
+ambilInputY = input("Masukan Nomor ke-2: ")
+op = input("tambah, kurang, kali, bagi? ")
+
+# Else-if untuk decision / penentuan output
+if (op.upper() == "TAMBAH"):
+  print(f'{ambilInputX} + {ambilInputY} = {tambah(int(ambilInputX), int(ambilInputY))}')
+elif (op.upper() == "KURANG"):
+  print(f'{ambilInputX} - {ambilInputY} = {kurang(int(ambilInputX), int(ambilInputY))}')
+elif (op.upper() == "KALI"):
+  print(f'{ambilInputX} x {ambilInputY} = {kali(int(ambilInputX), int(ambilInputY))}')
+elif (op.upper() == "BAGI"):
+  print(f'{ambilInputX} : {ambilInputY} = {bagi(int(ambilInputX), int(ambilInputY))}')
+else:
+  print("Error")
