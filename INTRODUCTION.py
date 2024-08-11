32132d1082339a62868f42ba99628e6b9a096ddd
Yami98 <chibakenta123@gmail.com>
2024-08-11 12:11:43 +0700

    Update INTRODUCTION.py

--- INTRODUCTION.py
+++ INTRODUCTION.py
@@ -17,3 +17,23 @@ elif friend_request == "no":
         print("AWHHH I HATE YOU!!!!")
      else:
         print("so it means you want..? YAY! I'm so happy!!")
+
+#===INTRODUCTION===#
+
+name = input("Hello! please enter your name below")
+print(f"Nice to meet you, {name}! how about we introduce ourselves more?")
+age = input("How old are you?")
+bot_age = 16
+age_difference = int(age) - int(bot_age)
+print(f"Oh my! we have {age_difference} age gap! I'm only 16, teehe!~")
+color = input("oh right!! what is your favorite color??")
+print(f"OH!!! {color} is also my favorite color!!")
+friend_request = input("do you want to be my friend??")
+if friend_request == "yes":
+    print("YAY! I'm so happy!!")    
+elif friend_request == "no":
+     confirmation = input("awhhh are you sure?? :(")
+     if confirmation == "yes":
+        print("AWHHH I HATE YOU!!!!")
+     else:
+        print("so it means you want..? YAY! I'm so happy!!")
