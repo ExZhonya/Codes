216381f16ffec9a6de9357cf73c258dd01588dc7
ExZhonya <exzhonya@gmail.com>
2024-08-13 20:24:49 +0700

    Update map.py

--- Game/map.py
+++ Game/map.py
@@ -28,9 +28,6 @@ class map1:
         
  def vil_quest():
   	while True:
-	  	grassland_quest = False
-	  	forest_quest = False
-	  	cave_quest = False
 	  	print('\n\n\n\n\n\n\n\n')
 	  	print('-'*30)
 	  	print('Adventurer! Please help us eradicate monsters destroying our fields.. we will reward you!')
@@ -230,11 +227,7 @@ __        __
 
  def forest():
     while True:
-        print('\n\n\n\n\n')
-        print('-'*30)
-        print("You're in the Forest")
-        print("[1]Explore [2]Grassland, [3]Cave, [4]Go Deeper")
-        print('-'*30)
+        preset.area("You're in Forest", "[1]Explore, [2]Cave, [3]Deep Forest")
         choice = input()
         if choice == "1":
             pass
@@ -247,11 +240,7 @@ __        __
 
  def cave():
     while True:
-        print('\n\n\n\n\n')
-        print('-'*30)
-        print("You're in the Cave")
-        print("[1]Explore, [2]Go Deeper, [3]Go Back")
-        print('-'*30)
+        preset.area("You're in Cave", "[1]Explore, [2]Deep Cave, [3]Forest")
         choice = input()
         if choice == "1":
             pass
@@ -261,28 +250,20 @@ __        __
             map1.forest()
 
  def deep_cave():
-    while True:
-        print('\n\n\n\n\n')
-        print('-'*30)
-        print("You're in the Deep Cave")
-        print("[1]Explore, [2]Go Deeper, [3]Go Back")
-        print('-'*30)
-        choice = input()
-        if choice == "1":
-            pass
-        elif choice == "2":
-            pass
-        elif choice == "3":
-           map1.cave()
+        while True:
+            preset.area("You're in Deep Cave", "[1]Explore, [2]Go Deeper, [3]Go Back")
+            choice = input()
+            if choice == "1":
+                pass
+            elif choice == "2":
+                pass
+            elif choice == "3":
+            map1.cave()
     
 
  def deep_forest():
     while True:
-        print('\n\n\n\n\n')
-        print('-'*30)
-        print("You're in the Deep Forest")
-        print("[1]Go Deeper, [2]Go Back")
-        print('-'*30)
+        preset.area("You're in Deep Forest", "[1]Explore, [2]Go Back")
         choice = input()
         if choice == "1":
             pass
@@ -291,27 +272,16 @@ __        __
 
  def grassland():
     while True:
-        print('\n\n\n\n\n')
-        print('-'*30)
-        print("You're in the Grassland")
-        print("[1]Village [2]Forest, [3]Mountain")
-        print('-'*30)
-        choice = input()
+        preset.area("You're in Grassland", "[1]Village [2]Forest,")
         if choice == "1":
             map1.village()
         elif choice == "2":
             map1.forest()
-        elif choice == "3":
-            pass
 
 class map2:
     def city():
       while True:
-        print('\n\n\n\n\n')
-        print('-'*30)
-        print("You're in the City")
-        print("[0]Guild, [1]Village, [2]Shop, [3]Blacksmith [4]City Sewers, [5]Dark Forest")
-        print('-'*30)
+        preset.area("You're in City", "[0]Guild, [1]Village, [2]Shop, [3]Blacksmith [4]City Sewers, [5]Dark Forest")
         choice = input()
         if choice == "0":
         	pass #will make guild def later
@@ -329,24 +299,8 @@ class map2:
            map3.castle()
 
     def city_shop():
-        print('\n\n\n\n\n')
-        print('-'*30)
-        print("You're in the City Shop")
-        print("[1]Buy, [2]Exit")
-        print('-'*30)
-        choice = input()
-        if choice == "1":
-            map2.in_city_shop()
-        elif choice == "2":
-            map2.city()
-
-    def in_city_shop():
         while True:
-            print('\n\n\n\n\n')
-            print('-'*30)
-            print("You're in the City Shop")
-            print('[1]Weapons, [2]Armor, [3]Potions, [4]Back')
-            print('-'*30)
+            preset.area("You're in City Shop", "[1]Weapon, [2]Armor, [3]Potion, [4]Back")
             choice = input()
             if choice == "1":
                 map1.vil_weap()
@@ -389,24 +343,17 @@ class map2:
 
     def dark_forest():
         while True:
-            print('\n\n\n\n\n')
-            print('-'*30)
-            print("You're in the Dark Forest")
-            print("[1]City, [3]Go Deeper")
+            preset.area("You're in Dark Forest", "[1]City, [2]Deep Dark Forest")
             print('-'*30)
             choice = input()
             if choice == "1":
                 map1.city()
-            elif choice == "3":
+            elif choice == "2":
                 map1.deep_dark_forest()
 
     def deep_dark_forest():
         while True:
-            print('\n\n\n\n\n')
-            print('-'*30)
-            print("You're in the Deep Dark Forest")
-            print("[1]Go Back, [3]Go Deeper")
-            print('-'*30)
+            preset.area("You're in Deep Dark Forest", "[1]City, [2]Go Back")
             choice = input()
             if choice == "1":
                 map1.dark_forest()
@@ -416,11 +363,7 @@ class map2:
 class map3:
     def castle():
         while True:
-            print('\n\n\n\n\n')
-            print('-'*30)
-            print("You're in the Castle")
-            print("[0]Guild, [1]City, [2]Shop, [3]Blacksmith, [4]Sewer [5]Cemetery [6]Mountains")
-            print('-'*30)
+            preset.area("You're in Castle", "[0]Guild, [1]City, [2]Shop, [3]Blacksmith [4]Sewers")
             choice = input()
             if choice == "0":
             	pass #city guild
@@ -432,11 +375,7 @@ class map3:
 
     def sewer():
      while True:
-        print('\n\n\n\n\n')
-        print('-'*30)
-        print("You're in the Sewer")
-        print("[1]Go Back, [2]Go Deeper")
-        print('-'*30)
+        preset.area("You're in Sewer", "[1]Go Back, [2]Go Deeper")
         choice = input()
         if choice == "1":
             map3.castle()
