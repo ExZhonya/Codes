b850849eeba7b6b64ee9be463fea3c375d7abff9
ExZhonya <exzhonya@gmail.com>
2024-09-10 16:37:09 +0700

    deletes

--- Game/ascii_preset.py
+++ Game/ascii_preset.py
@@ -119,52 +119,6 @@ class secret:
                 break
             else:
                 break
-
-class Player:
-    def __init__(self, name, hp, mp, strength, defense, level):
-        self.name = name
-        self.hp = hp
-        self.mp = mp
-        self.strength = strength
-        self.defense = defense
-        self.level = level
-
-    def display_stats(self):
-        print(f"{'='*30}")
-        print(f"Character: {self.name}")
-        print(f"{'='*30}")
-        print(f"{'Stat':<10} | {'Value':<5}")
-        print(f"{'-'*30}")
-        print(f"{'Health (HP)':<10}| {self.hp:<5}")
-        print(f"{'Mana (MP)':<10} | {self.mp:<5}")
-        print(f"{'Strength':<10} | {self.strength:<5}")
-        print(f"{'Defense':<10} | {self.defense:<5}")
-        print(f"{'Level':<10} | {self.level:<5}")
-        print(f"{'='*30}")
-
-#stats and the variable, to activate it just put player.display_stats()
-player = Player(name="Hero", hp=100, mp=50, strength=20, defense=15, level=5)
-
-class mob_table():
-    def preset():
-        hp = 210  # Current HP as an integer (percentage)
-        max_hp_bar_length = 21  # Total number of bars
-
-        filled_bars = hp // 10  # Each bar represents 10%
-
-        # Create the HP bar based on the filled bars
-        maxHP_bar = "[{}{}]".format('▆' * filled_bars, ' ' * (max_hp_bar_length - filled_bars))
-
-        print(f"""
-                ==============================
-                | Goblin     |  Lv.1         |
-                ==============================
-                | HP: {maxHP_bar}|
-                ==============================
-                |[1]Attack, [2]Defend, [3]Run|
-                ==============================
-                """)
-
 shop_v = Shop()
 weapon_v = Weapons()
 armor_v = Armors()
