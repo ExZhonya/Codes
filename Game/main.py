2b1a449815c704007c902ece0ffc01481df70adc
ExZhonya <exzhonya@gmail.com>
2024-10-19 11:53:51 +0700

    fixed damage, added fluc dmg, added base damage

--- Game/main.py
+++ Game/main.py
@@ -5,9 +5,9 @@ import monster_pick as monster
 from explore_handler import handle_single
 
 if __name__ == '__main__':
-    w.welc_ascii()
-    w.confirm()
-    c.spawn()
+    #w.welc_ascii()
+    #w.confirm()
+    #c.spawn()
 
     current_monster = monster.goblin
     handle_single(current_monster)
